d2dd46047576fe8df84c9d53bb3976775ac9d2e0
donemilio <donemilio@ya.ru>
2019-07-31 18:05:37 +0300

    445

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var app = express();
-
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
+app.set("view engine", "ejs");
+var data = "ololo";
+app.get("/",  (req, res) => res.render("index", {data: data} ));
 
 app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
