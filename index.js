8f6e6b294e749ec20449f3e0a817873c0ed0336a
donemilio <donemilio@ya.ru>
2019-08-02 03:27:36 +0300

    uyt

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // eslint-disable-next-line no-undef
 var express = require("express");
+// eslint-disable-next-line no-undef
+const bodyParser = require("body-parser");
+
 var app = express();
+var data = "hi";
 app.set("view engine", "ejs");
-var data = "/ololoiiiii";
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/",  (req, res) => res.render("index", {data: data} ));
 app.get("/create",  (req, res) => res.render("create" ));
+app.get("/create", (req, res) => {console.log(req.body);} );
 
 app.listen(3000, function () {
   console.log("Example app listening on port 3000!");
 });
+
